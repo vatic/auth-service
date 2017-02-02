6d8971d7c26b6473db394828e0a1efa9faaedf76
Sergey Vatagin <s.vatagin@gmail.com>
2017-02-02 23:19:52 +0300

    add mongodb connector and koa listen

--- src/app.js
+++ src/app.js
@@ -1,19 +1,53 @@
 // @flow
 const amqp = require('amqplib/callback_api');
-const { error } = require('./utils/logger');
+const MongoClient = require('mongodb').MongoClient;
+const Koa = require('koa');
+const cors = require('kcors');
+
+const { log, error } = require('./utils/logger');
 
 const { usersController } = require('./controllers');
 
-function fail(err) {
-  error('AMQP Error: %s', err);
+function fatal(msg, err) {
+  error(msg);
+  error(err);
   process.exit(1);
 }
 
-const app = (config/* : Object */) => {
-  amqp.connect(config.amqp.host, (err, conn) => {
-    if (err != null) fail(err);
-    usersController.start(conn);
+const connectDb = (host, port, db) => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(`mongodb://${host}:${port}/${db}`, (err, database) => {
+      if (err) reject(err);// fatal('MongoDB connect error.', err);
+      resolve(database);
+    });
   });
+};
+
+const connectRabbitMQ = (host) => {
+  return new Promise((resolve, reject) => {
+    amqp.connect(host, (err, conn) => {
+      if (err != null) reject(err);// fatal('AMQP connect error.', err);
+      resolve(conn);
+    });
+  });
+};
+
+
+const app = async (config/* : Object */) => {
+  // let db;
+  const koaApp = new Koa();
+  const { http, amqp: { host: amqpHost }, mongodb } = config;
+  // Initialize connection once
+  const db = await connectDb(mongodb.host, mongodb.port, mongodb.db);
+  const rmq = await connectRabbitMQ(amqpHost);
+
+  // response
+  koaApp.use(cors());
+  usersController.startHttp(koaApp);
+  koaApp.listen(http.port, () => log(`server started ${http.port}`));
+
+  usersController.startRmq(rmq);
+  usersController.startDb(db);
 };
 
 module.exports = app;
--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -1,7 +1,19 @@
 const { info } = require('../utils/logger');
+const Router = require('koa-router');
+
+const router = new Router({ prefix: '/auth' });
+
+// eslint-disable-next-line no-unused-vars
+router.get('/', async (ctx, next) => {
+  // eslint-disable-next-line no-param-reassign
+  ctx.body = {
+    route: 'auth',
+    msg: 'This is Auth route',
+  };
+});
 
 module.exports = {
-  start: (conn) => {
+  startRmq: (conn) => {
     const q = 'rabbit.test.vatagin';
     conn.createChannel((chErr, ch) => {
       ch.assertQueue(q, { durable: false });
@@ -12,4 +24,17 @@ module.exports = {
       }, { noAck: true });
     });
   },
+  startHttp: (koaApp) => {
+    koaApp.use(router.routes());
+    koaApp.use(router.allowedMethods());
+  },
+  startDb: (db) => {
+  },
 };
+
+class UsersController {
+  index() {
+    const users = UserModel.getAll();
+
+  } 
+}
